ebb8850270ecd67daea07f73ea74d26b60a93a66
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-03 16:16:18 -0400

    Added components and containers

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import classes from './Layout.module.scss';
+
+const layout = (props) => {
+  return (
+    <Fragment>
+      <div>Toolbar, Backdrop, Sidedrawer</div>
+      <main className={classes.content}>{props.children}</main>
+    </Fragment>
+  );
+};
+
+export default layout;
