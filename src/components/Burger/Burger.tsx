b5eeeffb1683ccdce45399fbf95194cce1a57f59
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-03 18:57:24 -0400

    Add empty array check in burger

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -4,11 +4,19 @@ import classes from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients: JSX.Element[] | JSX.Element = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <div>Please start adding ingredients!</div>;
+  }
 
   return (
     <div className={classes.Burger}>
