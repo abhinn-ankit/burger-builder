80162d9f456b41ba02b6dac2468c515a008ed953
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-03 16:44:33 -0400

    Update script

--- update.sh
+++ update.sh
@@ -2,6 +2,20 @@
 
 if [[ -z "$1" ]]
 then
-    echo "usage ./update.sh <commit message>"
+    echo "usage ./update.sh <commit message> <optional: true (to push to a new branch)>"
 fi
 
+echo -e "Adding files:\n"
+git add --all
+
+echo -e "Committing changes to local.\n"
+git commit -m "$1"
+
+echo -e "Pushing repo.\n"
+if [[ -z "$2" ]]
+then
+    branch_name=$(git branch | grep \* | cut -d ' ' -f2)
+    git push -u origin $branch_name
+else
+    git push
+fi
