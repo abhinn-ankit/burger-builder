86720e0f28109d5ad1645e6326435890cc158ac4
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-27 20:09:16 -0400

    Add axios post req

--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Abhinn',
+        address: {
+          street: 'Smith Street',
+          zipCode: '02120',
+          country: 'US',
+        },
+        emailAddress: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+    axios
+      .post('/orders.json', order)
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
   };
 
   render() {
