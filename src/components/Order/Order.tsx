f1b14616aec628234cd3e1431b83250255079d3e
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-29 14:50:52 -0400

    Transformed order props in order component

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -3,9 +3,28 @@ import PropTypes from 'prop-types';
 import classes from './Order.module.scss';
 
 const order = props => {
+    const ingredients = [];
+    for (const ingredientName in props.ingredients) {
+        ingredients.push({ name: ingredientName, amount: props.ingredients[ingredientName] });
+    }
+
+    const ingredientOutput = ingredients.map(ig => (
+        <span
+            key={ig.name}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '',
+                border: '1px solid #ccc',
+                padding: '5px',
+            }}
+        >
+            {ig.name} (ig.amount)
+        </span>
+    ));
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1) </p>
+            <p>Ingredients: {ingredientOutput}</p>
             <p>
                 Price: <strong>USD {props.price.toFixed(2)}</strong>
             </p>
