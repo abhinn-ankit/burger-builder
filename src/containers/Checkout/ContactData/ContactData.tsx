01526d1a0286510ae20b89f3c04dc378a7bec36e
Abhinn Ankit <abhinnankit@outlook.com>
2019-04-29 15:18:25 -0400

    add input component

--- src/containers/Checkout/ContactData/ContactData.tsx
+++ src/containers/Checkout/ContactData/ContactData.tsx
@@ -4,6 +4,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.scss';
 import axios from '../../../axios-orders';
 import { Route } from 'react-router-dom';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component<IProps & Route, any> {
     state = {
@@ -51,10 +52,10 @@ class ContactData extends Component<IProps & Route, any> {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
                 <Button btnType="Success" clicked={this.orderHandler}>
                     ORDER
                 </Button>
